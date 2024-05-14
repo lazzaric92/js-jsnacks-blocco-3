e53ada669a307d5311c8a139c53e3df89d268f80
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-14 17:38:08 +0200

    saved result in a constant, added if statements

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,2 +1,10 @@
 const fruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
 fruits.push('pesca');
+
+const result = fruits.find(ingredient => ingredient === 'cocomero');
+
+if(result) {
+    console.log('Trovato! Devo solo preparare il cocktail.');
+} else {
+    console.log('Oh no, devo uscire a comprare il cocomero!');
+}
