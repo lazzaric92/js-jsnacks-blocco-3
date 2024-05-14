9037955aab59654f357c8f6a833e7d7152ab6c12
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-14 18:02:00 +0200

    added for cycle to generate random numbers

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -4,6 +4,9 @@ let userNumber = Number.parseInt(prompt('Type a number'), 10);
 if(userNumber > 20 || userNumber < 1 || Number.isNaN(userNumber)) {
     console.log('Errore! Ti verrà assegnato un numero di default')
     userNumber = 10;
-} else {
-    // todo scrivo un ciclo for e pusho nell'array i  numeri generati randomicamente
 }
+
+for(let i = 0; i < userNumber; i++){
+    numbersArray.push(Math.floor(Math.random() * 100) + 1);
+}
+console.log(numbersArray);
