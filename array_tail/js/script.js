175c261179fd1c418e78f02703f2f8ac2e608855
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-14 18:20:47 +0200

    added bonus content

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -2,16 +2,18 @@ const numbersArray = [];
 let userNumber = Number.parseInt(prompt('Type a number'), 10);
 
 if(userNumber > 20 || userNumber < 1 || Number.isNaN(userNumber)) {
-    console.log('Errore! Ti verrà assegnato un numero di default')
-    userNumber = 10;
+    console.log('Warning! You will be assigned a default number')
+    userNumber = 15;
 }
 
 for(let i = 0; i < userNumber; i++){
     numbersArray.push(Math.floor(Math.random() * 100) + 1);
 }
 
-if(numbersArray.length >= 5){
-    console.log(numbersArray, numbersArray.slice(-5));
+const loggedNumbers = Number.parseInt(prompt('How many numbers do you want to log?'), 10);
+
+if(numbersArray.length < loggedNumbers || loggedNumbers < 0 || Number.isNaN(loggedNumbers)){
+    console.log('Warning! Not enough elements in the array or invalid number'); 
 } else {
-    console.log(numbersArray);
+    console.log(numbersArray.slice(-loggedNumbers));
 }
