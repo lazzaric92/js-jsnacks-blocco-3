df08370819ae1600dbb123899a7c19c5d47c0473
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-15 12:43:25 +0200

    added snack-11

---
+++ snack-11/js/script.js
@@ -0,0 +1,12 @@
+//todo: Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+const numbersArray = [];
+let sum = 0; 
+while (sum < 50){
+    const userNumber = Number.parseInt(prompt('Type a number'), 10);
+    if(!Number.isNaN(userNumber)){
+        numbersArray.push(userNumber);
+        sum += userNumber;
+    }
+}
+console.log(numbersArray, sum);
