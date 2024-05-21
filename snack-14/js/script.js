5f744479422c52d6e38bab29253c30f71b04439b
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-21 12:36:28 +0200

    added snack-14

---
+++ snack-14/js/script.js
@@ -0,0 +1,17 @@
+//todo: Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
+
+let userSeconds = Number.parseInt(prompt('Type the number of seconds of your timer') ,10);
+if(userSeconds < 0 || userSeconds > 30 || Number.isNaN(userSeconds)){
+    userSeconds = 10;
+    console.log('Given default timer:' + userSeconds);
+}
+
+const userTimer = setInterval(function(){
+    userSeconds--;
+    console.log(userSeconds);
+    if(userSeconds <= 0){
+        clearInterval(userTimer);
+        alert('Time is over!');
+    }
+}, 1000);
+
