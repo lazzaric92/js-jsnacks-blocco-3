8a921b76af9c1f505fb07cce424b2b2ffcede355
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-23 13:09:13 +0200

    revised snack-obj-2

--- snack-obj-2/js/script.js
+++ snack-obj-2/js/script.js
@@ -1,67 +1,75 @@
-//todo: Crea un array di oggetti che rappresentano delle persone, 10. Ogni persona ha un nome, un cognome e un'età.
+//todo: Crea un array di oggetti che rappresentano delle persone, 10. Ogni persona ha un nome, un cognome e un'eta.
 //todo: Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
 
 const users = [
     {
         nome: 'marco',
         cognome: 'rossi',
-        età: 14
+        eta: 14
     },
     {
         nome: 'dr',
         cognome: 'culocane',
-        età: 64
+        eta: 64
     },
     {
         nome: 'bruco',
         cognome: 'gianluco',
-        età: 17
+        eta: 17
     },
     {
         nome: 'mariangiongiangela',
         cognome: 'asd',
-        età: 48
+        eta: 48
     },
     {
         nome: 'fuffos',
         cognome: 'bau',
-        età: 9
+        eta: 9
     },
     {
         nome: 'ermenegilda',
         cognome: 'bellicapelli',
-        età: 34
+        eta: 34
     },
     {
         nome: 'ezio',
         cognome: 'auditore',
-        età: 24
+        eta: 24
     },
     {
         nome: 'rogiorzo',
         cognome: 'smith',
-        età: 16
+        eta: 16
     },
     {
         nome: 'nonno',
         cognome: 'laser',
-        età: 80
+        eta: 80
     },
     {
         nome: 'elena',
         cognome: 'guffo',
-        età: 18
+        eta: 18
     }
 ]
+
 
 const newArray = [];
 
 users.forEach(element => {
-    if(element.età >= 18){
+    if(element.eta >= 18){
         newArray.push(`${element.nome} ${element.cognome} può guidare`);
     } else {
         newArray.push(`${element.nome} ${element.cognome} non può guidare`);
     }
 });
 
+/*
+const newArray = users.map((persona) => {
+    return `${persona.nome} ${persona.cognome} ${(persona.eta >= 18) ? "può guidare" : "non può guidare"}`;
+})
+*/
+
 console.log(newArray);
+
