653a6347a8802909c95e040fa6f774090980ca4a
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-26 22:36:37 +0200

    added if statement for activeTab property

--- snack-obj-4/js/script.js
+++ snack-obj-4/js/script.js
@@ -42,34 +42,41 @@ const socials = ["Facebook", "GitHub", "Instagram", "Twitter"]
 
 
 let clonedBrowserWindows = [...browserWindows];
-console.log(clonedBrowserWindows);
+console.log(browserWindows);
+
 
 clonedBrowserWindows.forEach(element => {
+    // ? il software deve controllare
+    // ? se tra gli elementi di tab ci sono elementi che fanno parte dell'array socials
+        // > se sì rimuoverli
     (element["tab"]) = (element["tab"]).filter((tab) => {   // <-- filtro gli elementi dell'array tab
-        // ? il software deve controllare
-        // ? se tra gli elementi di tab ci sono elementi che fanno parte dell'array socials
-            // > se sì rimuoverli
         
         let isFound = false;
         socials.forEach(social => {   // <-- controllo ogni elemento dell'array social
-            if(isFound === false && social.toLowerCase() === tab.toLowerCase()){    // <-- vado avanti finché non trovo un elemento dell'array socials uguale a quello dell'array tab in esame
-                isFound = true;
-                console.log(tab + ' è un social');
+            if(social.toLowerCase() === tab.toLowerCase()){    // <-- se trovo un elemento dell'array socials uguale a quello dell'array tab in esame
+                isFound = true;    // <-- lo notifico ed esco dal ciclo
+                // console.log(tab + ' è un social');
                 return isFound;
             }
         });
-        return !isFound;
+        return !isFound;    // <-- se non l'ho trovato allora può passare il filtro, se l'ho trovato deve essere trattenuto
     
-        // ? quale è l'indice della tab attiva
-            // ? controllare che l'indice non sia più alto del numero di tab dopo la pulizia
-                // > se sì, portare l'indice a 0
-            // ? se l'indice era associato a un social 
-                // > se sì, attivare quello della tab successiva (considerando la pulizia)
-                // > se no, lasciarlo associato alla tab che era aperta (considerando la pulizia)
     })
-    console.log(clonedBrowserWindows);
+    
+    // ? quale è l'indice della tab attiva
+        // ? controllare che l'indice non sia più alto del numero di tab dopo la pulizia
+        if (element["activeTab"] > (element["tab"]).length){
+            // > se sì, portare l'indice a 0
+            element["activeTab"] = 0;
+        }
+
+        // ? se l'indice era associato a un social 
+            // > se sì, attivare quello della tab successiva
+            // > se no, lasciarlo associato alla tab che era aperta
+            // > ma, avendo chiuso quello prima, l'indice rimane uguale
 });
 
+console.log(clonedBrowserWindows);
 
 
 
